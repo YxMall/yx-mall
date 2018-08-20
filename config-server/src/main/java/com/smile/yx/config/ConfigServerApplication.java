c7672d82e36e2ea26920ce770d094bf054087fef
qing.wang <qing.wang.o@nio.com>
2018-08-20 13:47:42 +0800

    Initial commit

---
+++ config-server/src/main/java/com/smile/yx/config/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.smile.yx.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author qing.wang.o
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
---
+++ eureka-server/src/main/java/com/smile/yx/eureka/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.smile.yx.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author qing.wang.o
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
