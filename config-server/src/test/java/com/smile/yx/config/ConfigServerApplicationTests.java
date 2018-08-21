64b549a345a850c242ff4ead5b9aaad578856482
qing.wang <qing.wang.o@nio.com>
2018-08-21 09:49:36 +0800

    测试文件更改

--- config-server/src/test/java/com/smile/yx/config/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/smile/yx/config/ConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.smile.yx.eureka;
+package com.smile.yx.config;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EurekaServerApplicationTests {
+public class ConfigServerApplicationTests {
 
     @Test
     public void contextLoads() {
